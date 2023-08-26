fa2b26dcfb6f74b0991f5167fbb5e6e3611903c2
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2023-08-27 01:26:27 +0200

    Done challenge 2 of fundamentals 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -10,4 +10,12 @@ const dolphinsWinner = checkWinner(avgDolphins, avgKoalas);
 const koalasWinner = checkWinner(avgKoalas, avgDolphins);
 if (dolphinsWinner === true) console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`); 
 else if (koalasWinner === true) console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`); 
-else console.log(`No teams win`);
+else console.log(`No team wins`);
+
+// Challenge 2
+const calcTip = (bill) => (bill >= 50 && bill <= 300 ? 0.15 : 0.2) * bill;
+console.log(`Test bill: ${calcTip(100)}`);
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, total);
