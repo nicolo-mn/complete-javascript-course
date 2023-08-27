4093cd27c7750453a077122447aba8fa2b6cb36a
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2023-08-27 14:52:31 +0200

    Done challenge 4 of fundamentals 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,23 +1,23 @@
 'using strict';
 // Challenge 1
-const dolphinsScore1 = 44, dolphinsScore2 = 23, dolphinsScore3 = 71;
-const koalasScore1 = 65, koalasScore2 = 54, koalasScore3 = 49;
-const calcAverage = (p1,p2,p3) => (p1+p2+p3)/3;
-const avgDolphins = calcAverage(dolphinsScore1, dolphinsScore2, dolphinsScore3);
-const avgKoalas = calcAverage(koalasScore1, koalasScore2, koalasScore3);
-const checkWinner = (avg1, avg2) => avg1 >= 2 * avg2;
-const dolphinsWinner = checkWinner(avgDolphins, avgKoalas);
-const koalasWinner = checkWinner(avgKoalas, avgDolphins);
-if (dolphinsWinner === true) console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`); 
-else if (koalasWinner === true) console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`); 
-else console.log(`No team wins`);
+// const dolphinsScore1 = 44, dolphinsScore2 = 23, dolphinsScore3 = 71;
+// const koalasScore1 = 65, koalasScore2 = 54, koalasScore3 = 49;
+// const calcAverage = (p1,p2,p3) => (p1+p2+p3)/3;
+// const avgDolphins = calcAverage(dolphinsScore1, dolphinsScore2, dolphinsScore3);
+// const avgKoalas = calcAverage(koalasScore1, koalasScore2, koalasScore3);
+// const checkWinner = (avg1, avg2) => avg1 >= 2 * avg2;
+// const dolphinsWinner = checkWinner(avgDolphins, avgKoalas);
+// const koalasWinner = checkWinner(avgKoalas, avgDolphins);
+// if (dolphinsWinner === true) console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`); 
+// else if (koalasWinner === true) console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`); 
+// else console.log(`No team wins`);
 
 // Challenge 2
 const calcTip = (bill) => (bill >= 50 && bill <= 300 ? 0.15 : 0.2) * bill;
 console.log(`Test bill: ${calcTip(100)}`);
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+let bills = [125, 555, 44];
+let tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+let total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(bills, tips, total);
 
 // Challenge 3
@@ -48,3 +48,24 @@ if (mark.calcBMI() >= john.calcBMI()) {
 } else if (john.bmi > mark.bmi) {
   console.log(`John's BMI (${john.BMI}) is higer than Mark's (${mark.BMI})!`);
 }
+
+// Challenge 4
+const calcAverage = function (arr) {
+  avg = 0
+  for (let i = 0; i < arr.length; i++) {
+    avg += arr[i] / arr.length;
+  }
+  return avg;
+}
+
+bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+tips = [];
+totals = [];
+for (let i = 0; i < bills.length; i++) {
+  tips[i] = calcTip(bills[i]);
+  totals[i] = bills[i] + tips[i];
+}
+
+console.log(`tips: ${tips}`);
+console.log(`totals: ${totals}`);
+console.log(`totals average: ${calcAverage(totals)}`);
