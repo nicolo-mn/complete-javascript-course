36d0fe54450be12a2a30a1c8f75d5d7497fef4bb
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2023-09-01 13:06:22 +0200

    Done challenge 1 of dev skills

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
 
+function printForecast(arr) {
+  let final = '...';
+  for (let i=0; i < arr.length; i++) {
+    final += ` ${arr[i]}°C in ${i+1} days ...`;
+  }
+  console.log(final);
+}
+
+printForecast(data2);
